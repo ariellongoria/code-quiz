233e05791b4f0c27b7a1a8bd85d61843c68bcd73
ariellongoria <ariello97@hotmail.com>
2021-04-18 21:57:39 -0500

    proper question display order and end screen

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# code-quiz
+# Code Quiz
+
+This is a simple quiz with coding related questions!
+
+Test your knowledge and get the high score! Your score is determined by your correct answers and incorrect answers, as answering incorrectly will penalize your time by ten seconds.
+
+Have fun learning!
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var questions = [
     {
         question: "Commonly used data types DO NOT include:",
         options: ["strings", "booleans", "alerts", "numbers"],
-        correctOptions: "alerts",
+        correctOption: "alerts",
     },
     {
         question: "The condition in an if / else statement is enclosed with __.",
@@ -20,33 +20,41 @@ var questions = [
         correctOption: "all of the above",
     },
     {
-        question: "A very useful toll used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
         correctOption: "console log",
     },
 ];
 
 var score;
+var questionIndex = 0;
+var timeLeft = 120;
+var highScores = [];
 
 var startButton = document.querySelector("#start-button");
 var startPage = document.querySelector("#start-page");
 var quizBox = document.querySelector(".quiz");
+var highScoreButton = document.querySelector("#high-scores-button");
+var highScoreScreen = document.querySelector(".high-scores");
+var countdownTimer = document.getElementById("time-left");
 
 // Starts timer
 var beginTimer = function () {
-    let timeLeft = 120;
-    var countdownTimer = document.getElementById("time-left");
-    setInterval(function () {
-        if (timeLeft <= 0) {
-            clearInterval((timeLeft = 0));
+    var newTimer = setInterval(function () {
+        if (timeLeft < 0) {
+            clearInterval(newTimer);
+            endQuiz();
+        } else if (questionIndex >= questions.length) {
+            clearInterval(newTimer);
+        } else {
+            countdownTimer.innerHTML = timeLeft + " seconds";
+            timeLeft -= 1;
         }
-        countdownTimer.innerHTML = timeLeft + " seconds";
-        timeLeft -= 1;
     }, 1000);
 };
 
 //Initializes the quiz structure
-var initialize = function () {
+var initializeQuestions = function () {
     var newQuestion = document.createElement("h2");
     newQuestion.className = "question";
     quizBox.appendChild(newQuestion);
@@ -64,17 +72,82 @@ var initialize = function () {
 var renderQuestion = function (index) {
     document.querySelector(".question").textContent = questions[index].question;
     for (var i = 0; i < questions[index].options.length; i++) {
-        document.querySelector("button[answer-id = '" + i + "']").textContent = questions[index].options[i];
+        var currentAnswer = document.querySelector("button[answer-id = '" + i + "']");
+        currentAnswer.textContent = questions[index].options[i];
+        if (currentAnswer.textContent === questions[index].correctOption) {
+            currentAnswer.setAttribute("is-correct", true);
+        } else {
+            currentAnswer.setAttribute("is-correct", false);
+        }
     }
 };
 
-//pogchamp
+var submitScore = function (event) {
+    event.preventDefault();
+    var playerName = document.querySelector("input[name = 'player-name']");
+    var newHighScore = {
+        player: playerName.value,
+        score: timeLeft,
+    };
+    highScores.push(newHighScore);
+    saveScores();
+};
+
+var saveScores = function () {
+    localStorage.setItem("scores", JSON.stringify(highScores));
+};
+
+var renderHighScores = function () {};
+
+var endQuizScreen = document.querySelector(".end-screen");
+
+var renderEndScreen = function () {
+    var form = document.createElement("form");
+    var nameSubmission = document.createElement("input");
+    nameSubmission.setAttribute("type", "text");
+    nameSubmission.setAttribute("name", "player-name");
+    form.appendChild(nameSubmission);
+    var scoreSubmitButton = document.createElement("button");
+    scoreSubmitButton.textContent = "Submit New Score";
+    scoreSubmitButton.setAttribute("type", "submit");
+    form.appendChild(scoreSubmitButton);
+    endQuizScreen.appendChild(form);
+};
+
+var endQuiz = function () {
+    quizBox.innerHTML = "";
+    console.log(timeLeft);
+    countdownTimer.textContent = timeLeft;
+    renderEndScreen();
+};
+
+quizBox.addEventListener("click", function (event) {
+    console.log(questionIndex);
+    if (event.target.getAttribute("is-correct") === "true") {
+        console.log("poggers");
+    } else {
+        console.log("not poggers");
+        timeLeft -= 10;
+    }
+    if (questionIndex < questions.length - 1) {
+        questionIndex++;
+        renderQuestion(questionIndex);
+    } else {
+        questionIndex++;
+        console.log(questionIndex);
+        endQuiz();
+    }
+});
+
+//Starts the quiz
 var startQuiz = function () {
-    startPage.remove();
+    startPage.innerHTML = "";
     score = 0;
-    initialize();
-    renderQuestion(0);
+    initializeQuestions();
+    renderQuestion(questionIndex);
 };
 
 startButton.addEventListener("click", startQuiz);
 startButton.addEventListener("click", beginTimer);
+highScoreButton.addEventListener("click", renderHighScores);
+endQuizScreen.addEventListener("submit", submitScore);
